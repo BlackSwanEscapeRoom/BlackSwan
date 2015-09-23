c90fd772a197e3b2246ecf109c8d944b37f2d0f0
Roy Stijsiger <roy.stijsiger@outlook.com>
2015-09-23 12:20:33 +0200

    Added .ino xD

--- BlackSwan/BlackSwan.Arduino/BlackSwan.Arduino.ino
+++ BlackSwan/BlackSwan.Arduino/BlackSwan.Arduino.ino
@@ -1,13 +1,169 @@
-void setup()
-{
+/*
+  Web Server
 
-  /* add setup code here */
+ A simple web server that shows the value of the analog input pins.
+ using an Arduino Wiznet Ethernet shield.
 
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+ * Analog inputs attached to pins A0 through A5 (optional)
+
+ created 18 Dec 2009
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+ modified 02 Sept 2015
+ by Arturo Guadalupi
+
+ */
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <string.h>
+
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+
+
+IPAddress ip(192, 168, 1, 177);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+bool led = false;
+    
+void setup() {
+    pinMode(13,OUTPUT);
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
 }
 
-void loop()
-{
-
-  /* add main program code here */
-
+String handleMeta(){
+  return "handleMetaFunctie";
 }
+
+String handleSensor(){
+  return "handleSensorFucntie";
+}
+
+String handleRequest(String method, String path){
+
+  if(method == "GET"){
+      if(path == "/meta"){
+        return handleMeta();
+      }
+      else if(path == "/sensor/switch"){
+          return handleSensor();
+      }
+  }
+  else if(method == "POST"){
+    if(path == "/actions/led/1"){
+      led = true;
+      return "AAN";
+      //delay();
+    }
+    else if(path == "/actions/led/0"){
+        led = false;
+       return "UIT";
+    }
+    return "";
+  }
+  
+  return  "error" + method + path;
+}
+
+void loop(){
+  digitalWrite(13,LOW);
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    String test = String();
+    String method = String();
+    String path = String();
+ 
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    boolean methodIsSet = false;
+    boolean pathIsSet = true;
+    
+    int spaceCount = 0;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        test.concat(c);        
+        if(c == ' '){
+          spaceCount++;
+        }
+        else{
+          switch(spaceCount){
+            case 0:
+              method.concat(c);
+              break;
+            case 1:
+              path.concat(c);
+              break;
+          }
+        }
+          
+     
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  
+          // the connection will be closed after completion of the response
+          client.println();
+          //set method and path
+          
+
+          //String path = ;
+          test = "";
+    
+          //handle reqeuest open on screen
+          client.println(handleRequest(method,path));
+          Serial.println(method + path);
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+
+    
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+    Ethernet.maintain();
+  }
+}
+
+
+
